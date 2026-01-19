e18bc9891e7ee89eede9899ffdc868075816322c
SANTHOSH MATHAVAN R <santhoshr292003@gmail.com>
2026-01-19 15:38:32 +0530

    final commit

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,7 +1,5 @@
+// lib/firebase/firebaseApp.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
@@ -12,9 +10,5 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// Prevent re-initialization in Next.js
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const firebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
---
+++ app/lib/firebaseClient.ts
@@ -0,0 +1,11 @@
+// lib/firebase/firebaseClient.ts
+"use client";
+
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { firebaseApp } from "./firebase";
+
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
